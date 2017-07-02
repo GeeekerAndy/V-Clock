3bec66d12185a91eb0cd530452188bfa48ce9252
GeekerAndyLove <GeekerAndy@outlook.com>
2017-07-02 11:30:35 +0800

    Add Volley.

--- Android/V-Clock/app/src/main/java/com/example/dell/v_clock/MainActivity.java
+++ Android/V-Clock/app/src/main/java/com/example/dell/v_clock/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.dell.v_clock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView mTextView = (TextView)findViewById(R.id.text);
+
+        //Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://www.google.com/ncr";
+
+        //Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        mTextView.setText("Response is: " + response.substring(0, 500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+
+        //Add the request to the RequestQueue
+        queue.add(stringRequest);
+
     }
 }
