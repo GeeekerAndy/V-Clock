d25c0418092358eb8f4e736920698385eb1122fb
GeeerAndyLove <GeekerAndy@outlook.com>
2017-07-03 17:38:05 +0800

    Finish basic employee register UI and test POST success.

--- Android/V-Clock/app/src/main/java/com/example/dell/v_clock/activity/LoginActivity.java
+++ Android/V-Clock/app/src/main/java/com/example/dell/v_clock/activity/LoginActivity.java
@@ -1,9 +1,30 @@
 package com.example.dell.v_clock.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.dell.v_clock.R;
+
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This activity is the interface for the stuff to login which can jump to RegisterActivity.
@@ -16,5 +37,70 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button tempButton = (Button)findViewById(R.id.bt_temp_button);
+        tempButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+/*
+Just test function.
+ */
+        final TextView tempTextView = (TextView)findViewById(R.id.tv_temp_show_json);
+        RequestQueue queue = Volley.newRequestQueue(this);
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest("http://httpbin.org/delay/3", null,
+//                new Response.Listener<JSONObject>() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        tempTextView.setText(response.toString());
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Log.e("TAG", error.getMessage(), error);
+//            }
+//        });
+//        queue.add(jsonObjectRequest);
+
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://121.250.222.39:8080/VClock_wolf/servlet/RegisterServlet", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                tempTextView.setText(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                tempTextView.setText(error.toString());
+            }
+        }) {
+//            @Override
+//            public byte[] getBody() throws AuthFailureError {
+//                String httpPostBody;
+//                String httpPostBody2;
+//                try {
+//                    httpPostBody = "12345678910";
+//                    httpPostBody2 = "12345678910"+ URLEncoder.encode("{{%stuffToBe Escaped/","UTF-8");
+//                } catch (UnsupportedEncodingException exception) {
+//                    Log.e("ERROR", "exception", exception);
+//                    return null;
+//                }
+//                return httpPostBody2.getBytes();
+//            }
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map = new HashMap<String, String>();
+                map.put("etel", "12345678910");
+                map.put("etel1", "12345678911");
+                return map;
+            }
+        };
+        queue.add(stringRequest);
     }
 }
--- Android/V-Clock/app/src/main/java/com/example/dell/v_clock/activity/RegisterActivity.java
+++ Android/V-Clock/app/src/main/java/com/example/dell/v_clock/activity/RegisterActivity.java
@@ -1,9 +1,26 @@
 package com.example.dell.v_clock.activity;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.dell.v_clock.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * The staff can register account here.
@@ -11,9 +28,66 @@ import com.example.dell.v_clock.R;
 */
 public class RegisterActivity extends AppCompatActivity {
 
+    private AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.8F);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final RequestQueue requestQueue  = Volley.newRequestQueue(this);
+
+        final EditText employeeName = (EditText)findViewById(R.id.et_employee_name);
+        RadioGroup sexGroup = (RadioGroup)findViewById(R.id.rg_sex_group);
+        final RadioButton sexMan = (RadioButton)findViewById(R.id.rb_sex_man);
+        RadioButton sexWoman = (RadioButton)findViewById(R.id.rb_sex_woman);
+        final EditText employeePhone = (EditText)findViewById(R.id.et_employee_phone);
+        final JSONObject employeeInfo = new JSONObject();
+
+        sexMan.setChecked(true);
+
+        Button goToSelectRegisterPhoto = (Button)findViewById(R.id.bt_go_to_select_register_photo);
+        goToSelectRegisterPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.startAnimation(buttonClick);
+                if(employeeName.getText().toString().equals("")) {
+                    Toast.makeText(RegisterActivity.this, "姓名为空!", Toast.LENGTH_SHORT).show();
+                } else if(employeePhone.getText().length() != 11) {
+                    Toast.makeText(RegisterActivity.this, "手机号格式错误！", Toast.LENGTH_SHORT).show();
+                } else {
+                    //The information of employee if legal. Judge whether employee tel is registered.
+                    // 数据已合法。判断工作人员手机是否被注册。
+                    String url = "http://192.168.2.101:80/serlet/loginServlet";
+                    StringRequest registerStat = new StringRequest(url, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+//                            if(requestQueue.equals())
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    });
+                    requestQueue.add(registerStat);
+
+
+                    //employee tel is not registered.
+                    try {
+                        employeeInfo.put("ename", employeeName.getText().toString());
+                        if(sexMan.isSelected()) {
+                            employeeInfo.put("esex", "男");
+                        } else {
+                            employeeInfo.put("esex", "女");
+                        }
+                        employeeInfo.put("etel", employeePhone.getText().toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
     }
 }
